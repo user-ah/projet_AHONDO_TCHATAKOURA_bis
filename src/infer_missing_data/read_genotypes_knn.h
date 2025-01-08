26c78f9a970e10fcc8953d242fec53a0854f31c8
E23A483M <ahamed.tchatakoura@etu.univ-nantes.fr>
2025-01-09 00:17:14 +0100

    Stats pour les data_set 1, 2, 3, 4 et 5 sont OK

--- src/infer_missing_data/read_genotypes_knn.h
+++ src/infer_missing_data/read_genotypes_knn.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <stdexcept>
 
 inline bool readGenotypes(const std::string &genotypes_file, std::vector<std::vector<int>> &genotypes) {
     std::ifstream file(genotypes_file);
@@ -13,22 +14,34 @@ inline bool readGenotypes(const std::string &genotypes_file, std::vector<std::ve
         std::cerr << "Erreur : Impossible d'ouvrir " << genotypes_file << std::endl;
         return false;
     }
+
     std::string line;
     while (std::getline(file, line)) {
         std::vector<int> row;
         std::stringstream ss(line);
         std::string cell;
-        
-        // Lire chaque cellule séparée par un espace
-        while (ss >> cell) {
-            if (cell == "NA") {  // Valeur manquante
-                row.push_back(-1);
-            } else {
-                row.push_back(std::stoi(cell)); // Convertir la cellule en entier
+
+        // Lire chaque cellule séparée par une virgule
+        while (std::getline(ss, cell, ',')) {
+            try {
+                if (cell == "NA") {  // Valeur manquante
+                    row.push_back(-1);
+                } else {
+                    row.push_back(std::stoi(cell));  // Convertir la cellule en entier
+                }
+            } catch (const std::invalid_argument &e) {
+                std::cerr << "Erreur : Cellule invalide '" << cell << "' dans le fichier " << genotypes_file << std::endl;
+                return false;
+            } catch (const std::out_of_range &e) {
+                std::cerr << "Erreur : Valeur hors plage dans la cellule '" << cell << "' dans le fichier " << genotypes_file << std::endl;
+                return false;
             }
         }
+
+        // Ajouter la ligne lue aux génotypes
         genotypes.push_back(row);
     }
+
     file.close();
     return true;
 }
