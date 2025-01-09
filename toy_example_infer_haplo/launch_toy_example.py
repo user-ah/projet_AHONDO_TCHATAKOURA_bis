41889451dd03d06ff04ffcb7ac4fe42fc9bfc21e
Mawuéna AHONDO <mawuenaahondo@etu-univ.com>
2025-01-09 15:51:21 +0100

    commentaire

--- toy_example_infer_haplo/launch_toy_example.py
+++ toy_example_infer_haplo/launch_toy_example.py
@@ -1,24 +1,46 @@
+"""
+    File: run_infer_haplo.py
+
+    Authors: Ahamed TCHATAKOURA & Mawuéna AHONDO
+
+    Description:
+    This script compiles and runs a C++ program for haplotype inference.
+    It first compiles the C++ source code, and if successful, it executes the compiled program with input and output files for genotypes, ground truth haplotypes, inferred haplotypes, and logs.
+
+    Key Features:
+    - Compiles the C++ source code using `g++` with C++17 standard.
+    - Executes the compiled program with input files: genotypes, ground truth haplotypes, and a specified output log file.
+    - Handles compilation and execution errors with appropriate error messages.
+    - Assumes that the C++ source code and necessary input files exist at specified paths.
+
+    Output:
+    - The script runs the compiled executable, generating a log file with the results of the haplotype inference.
+
+    Preconditions:
+    - The C++ source file (`infer_haplo.cpp`) must be written correctly and located at the specified path.
+    - The input files (`genotypes.csv`, `haplotypes.csv`) and the output log file must be specified and used correctly.
+"""
+
 import os
 import sys
 import subprocess
 
 # Variables pour les fichiers et paramètres
-genotypes_file = "../src/infer_haplo/genotypes.csv"
-groundtruth_file = "../src/infer_haplo/haplotypes.csv"
-InferedHaplotypes_file = "../src/infer_haplo/infered_haplo.csv"
-log_file = "../src/infer_haplo/log.txt"
-cpp_file = "../src/infer_haplo/infer_haplo.cpp"  # Fichier source C++
+genotypes_file = "../data/Dataset_1/genotypes.csv"
+groundtruth_file = "../data/Dataset_1/haplotypes.csv"
+InferedHaplotypes_file = "../data/Dataset_1//infered_haplo.csv"
+log_file = "../data/Dataset_1/log.txt"
+cpp_file = "../src/infer_haplo/infer_haplo.cpp"  
 
-# Nom de l'exécutable
-executable = "../src/infer_haplo/infer_haplo"
 
-# Commande de compilation pour le fichier C++
-compile_command = f"g++ -o {executable} {cpp_file}"
+executable = "../src/infer_haplo/infer_haplo_1"
 
-# Création de la commande
+
+compile_command = f"g++ -std=c++17 -o {executable} {cpp_file}"
+
+
 cmd = f"{executable} {genotypes_file} {groundtruth_file} {InferedHaplotypes_file} {log_file}"
 
-# Vérification de la compilation
 try:
     print(f"Compilation de {cpp_file}...")
     subprocess.run(compile_command, shell=True, check=True)
@@ -27,10 +49,10 @@ except subprocess.CalledProcessError as e:
     sys.stderr.write(f"Erreur lors de la compilation du fichier C++ : {cpp_file}\n{e}\n")
     sys.exit(1)
 
-# Affichage de la commande pour validation
+
 print(f"Exécution de la commande : {cmd}")
 
-# Exécution de la commande
+
 try:
     subprocess.run(cmd, shell=True, check=True)
     print("Exécution réussie.")
